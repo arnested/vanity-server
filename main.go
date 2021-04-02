34b528ac334be2da103de745ee34dbf3c538abcf
Arne Jørgensen <arne@arnested.dk>
2021-04-02 16:05:47 +0200

    With path strip

--- main.go
+++ main.go
@@ -10,25 +10,25 @@ import (
 func main() {
 	mappings := []vanity.Mapping{
 		vanity.Mapping{
-			Path:        "/go/dsupdate",
+			Path:        "/dsupdate",
 			URL:         "https://github.com/arnested/dsupdate",
 			Description: "dsupdate",
 			HideInIndex: false,
 		},
 		vanity.Mapping{
-			Path:        "/go/triagebot",
+			Path:        "/triagebot",
 			URL:         "https://github.com/arnested/triagebot",
 			Description: "triagebot",
 			HideInIndex: false,
 		},
 		vanity.Mapping{
-			Path:        "/go/healthy",
+			Path:        "/healthy",
 			URL:         "https://github.com/arnested/healthy",
 			Description: "healthy",
 			HideInIndex: false,
 		},
 		vanity.Mapping{
-			Path:        "/go/vanity-server",
+			Path:        "/vanity-server",
 			URL:         "https://github.com/arnested/vanity-server",
 			Description: "vanity-server",
 			HideInIndex: false,
@@ -38,7 +38,7 @@ func main() {
 	addr := ":80"
 	args := vanity.Args{
 		Config: vanity.Config{
-			Prefix:   "arnested.dk",
+			Prefix:   "arnested.dk/go",
 			Mappings: mappings,
 		},
 		NoIndex: false,
