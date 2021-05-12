c5e16fb44cda4f4ebd84e6c318f0811022efcc0a
Arne Jørgensen <arne@arnested.dk>
2021-05-12 22:24:24 +0200

    Add stricter HTTP headers

--- main.go
+++ main.go
@@ -65,7 +65,15 @@ func main() {
 
 	handler := vanity.Handler(args)
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Strict-Transport-Security", "max-age: 10886400; includeSubdomains; preload")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-Content-Type", "nosniff")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'unsafe-inline'")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		handler(w, r)
+	})
+
 	err = http.Serve(listener, nil)
 	if err != nil {
 		log.Fatal(err)
